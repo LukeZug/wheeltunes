016d68994647ab7e795ce318f3b2296d5ae4f937
Luke Mullen <lamullen20@gmail.com>
2023-02-20 10:49:49 +0000

    Model changes + population script added.

---
+++ wheeltunes/populate.py
@@ -0,0 +1,27 @@
+import os
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'wheeltunes.settings')
+
+import django
+django.setup()
+
+from wheeltunes_app.models import *
+import random
+
+NUM_OF_SONGS = 20
+
+def populate():
+    for i in range(NUM_OF_SONGS):
+        create_mock_song(i)
+
+def create_mock_song(i):
+    Song.objects.get_or_create(
+        title=f"Song number {i+1}",
+        artist=f"Artist {i+1}",
+        static_url="",
+        energy=random.randint(0, 100),
+        tempo=random.randint(50,150),
+        playable=True
+    )
+
+if __name__ == "__main__":
+    populate()
--- wheeltunes/wheeltunes_app/models.py
+++ wheeltunes/wheeltunes_app/models.py
@@ -3,12 +3,11 @@ from django.db import models
 class Song(models.Model):
     title = models.CharField(max_length=200)
     artist = models.CharField(max_length=200)
-    duration = models.IntegerField()
-    file = models.FileField()
-    energy = models.IntegerField()
-    tempo = models.IntegerField()
+    static_url = models.URLField(blank=True, null=True)
+    energy = models.IntegerField(default=0)
+    tempo = models.IntegerField(default=50)
+    playable = models.BooleanField(default=False)
     
-
     def __str__(self):
         return self.title
     
